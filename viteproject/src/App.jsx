b8166c4d16e158e8fd49e4adb2ea499049f87676
Akhil <saiakil456@gmail.com>
2023-08-03 00:06:56 +0530

    added nav styles

--- viteproject/src/App.jsx
+++ viteproject/src/App.jsx
@@ -6,13 +6,10 @@ import "primereact/resources/primereact.css"; // core css
 import "primeicons/primeicons.css"; // icons
 import "primeflex/primeflex.css";
 import "font-awesome/css/font-awesome.min.css";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./components/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/footer/footer";
-
-
 
 function App() {
   return (
@@ -20,17 +17,17 @@ function App() {
       {/* <div className="bg-cont">
         <Header />
       </div> */}
-       <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/:mediaType/:id" element={<Details />} />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/:mediaType/:id" element={<Details />} />
         <Route path="/search/:query" element={<SearchResult />} />
         <Route path="/explore/:mediaType" element={<Explore />} />
         <Route path="*" element={<PageNotFound />} /> */}
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
