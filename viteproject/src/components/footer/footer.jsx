d4206ddd149f2ae0cfb8fe290a4ba37fc42afb32
vishnu <vishnuvardhagangula123@gmail.com>
2023-08-05 20:58:26 +0530

    footer and flags card  css changes

--- viteproject/src/components/footer/footer.jsx
+++ viteproject/src/components/footer/footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
 import "./footer.css"
 import 'bootstrap/dist/css/bootstrap.css';
 import { AiFillTwitterCircle, AiFillYoutube, AiFillFacebook, AiFillInstagram } from 'react-icons/ai';
@@ -13,29 +12,29 @@ const Footer = () => {
         <div className="container">
         <div className='middle-crad '>
 
-          <div> <h6>IF YOU HAVE ANY QUREY OR NEED INFORMATION ABOUT STEM OVERSERAL PLEACE CONTACT</h6></div>
+          <div className='footer-subcard'  style={{fontWeight:400}}> <h6>IF YOU HAVE ANY QUREY OR NEED INFORMATION ABOUT STEM OVERSERAL PLEACE CONTACT</h6></div>
              
               <div className='sub-middle-card'>
                 <h6>+91 8106658662, 123456789</h6>
 
               </div>
             </div>
-          <div className="row">
-           
+          <div className="row total-footercard">
+          
             <div className="col-md-6 col-lg-4 col-12 ft-1">
               <img src="https://thumbs.dreamstime.com/b/kids-education-child-boy-study-school-thinking-bubble-dreaming-over-black-chalkboard-75214276.jpg" style={{ height: "100px", width: "100px" }} />
               <div className="footer-icons">
-                <a href=''> <AiFillTwitterCircle style={{ fontSize: "20px", color: "white",marginRight:"7px" }} /></a>
+                <a href=''> <AiFillTwitterCircle style={{ fontSize: "20px", color: "white",marginRight:"7px" }} className='icons'/></a>
                 <a href='' ><AiFillYoutube style={{ fontSize: "20px", color: "white",marginRight:"7px" }}  /></a>
                 <a href='' ><AiFillFacebook style={{ fontSize: "20px", color: "white",marginRight:"7px"  }} /></a>
                 <a href='' ><AiFillInstagram style={{ fontSize: "20px", color: "white" ,marginRight:"7px" }} /></a>
 
               </div>
             </div>
-            <div className="col-md-6 col-lg-3 col-12 ft-2">
-              <h5 className='main-heading'>  STUDY ABROAD</h5>
+            <div className="col-md-6 col-lg-3 col-12 ft-2 ">
+              <h5 className='main-heading'>  STUDY ABROAD</h5>                                      
               <ul >
-                <li className="nav-item">
+                <li className="nav-item">     
                   <a className="" href="/">Study in Uk</a>
                 </li>
                 <li className="nav-item">
@@ -84,6 +83,7 @@ const Footer = () => {
             <hr className='horizental'></hr>
           </div>
         </div>
+        
       </div>
       <div className='Last-footer'>
         <p>2023 Stem Overseas All Rights Reserved</p>
